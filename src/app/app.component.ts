538d061430240d9231ea3bb18b85bdb3272b252a
amchl <adrien.mchl@gmail.com>
2020-02-23 16:28:57 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Your app name';
+  name = 'Your app name'; // You can modify it ...
+  todolist; // My todolist...
+  // ...
+
+  addTask(){
+    // Add task to todolist...
+  }
+
+  removeTask(){
+    // Remove task from todolist...
+  }
+
+  doneTask(){
+    // Set task to done...
+  }
 }
